1e9713b156ea993af9a807bde9f714eaa5230a6f
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-02-09 01:52:42 +0530

    Create PassByReferencePointers.cpp

---
+++ Dynamic Memory/PassByReferencePointers.cpp
@@ -0,0 +1,20 @@
+/*Pass by reference using pointers*/
+#include<iostream>
+using namespace std;
+
+void applyTax(int * income){
+	float tax = 0.10;
+	*income = *income - (*income)*tax;
+}
+
+
+int main(){
+
+	int income;
+	cin>>income;
+
+	applyTax(income);
+
+	cout << income <<endl;
+
+}
