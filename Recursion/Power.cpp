e98a08e7a01b25ac55ce53e1986495b09844c19e
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-03-24 12:39:14 +0530

    Create Power.cpp

---
+++ Recursion/Power.cpp
@@ -0,0 +1,42 @@
+//Compute a^n
+
+#include<iostream>
+using namespace std;
+
+
+int power(int a,int n){
+	//base case
+	if(n==0){
+		return 1;
+	}
+	return a * power(a,n-1);
+}
+
+
+//If n is even: 2^6 = (2^3)^2
+//If n is odd: 2^7 = 2.(2^3)^2
+int fastPower(int a,int n){
+	if(n==0){
+		return 1;
+	}
+
+	int subProb = fastPower(a,n/2);
+	int subProbSq = subProb*subProb;
+	if(n&1){
+		return a*subProbSq;
+	}
+	return subProbSq;
+}
+
+
+
+int main(){
+
+	int a,n;
+	cin>>a>>n;
+	cout<<power(a,n) <<endl;
+	cout<<fastPower(a,n)<<endl;
+	
+	return 0;
+}
+
